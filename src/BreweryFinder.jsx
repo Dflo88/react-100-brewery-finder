1336662747913aa8eaf1dcd4c4e1036313caa4d5
Diego <dflores7237@gmail.com>
2020-05-11 15:13:42 -0700

    refactored breweryFinder class component into a functional componnet

--- src/BreweryFinder.jsx
+++ src/BreweryFinder.jsx
@@ -1,37 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class BreweryFinder extends Component {
-    constructor(props){
-        super(props);
-    };
-
-    handleBreweryQuickFind() {
-        this.props.handleBreweryQuickFind();
-    };
-
-    handleBreweryFindByUserInput() {
-        let userEnteredZipcode = document.getElementById('userEnteredZipcode').value;
-        this.props.handleBreweryFindByUserInput(userEnteredZipcode);
-    }
-
-    render(){
-        return(
-            <div>
-                <div className='lookupContainer'>
-                    <div className='autoBreweryFinder'>
-                        <p id='autoBreweryFinderText'>QUICK! Lets find a brewery close to you right...</p>
-                        <button className='quickFindButton' type='button' onClick={this.handleBreweryQuickFind.bind(this)}> NOW!</button>
-                    </div>
-                    <div className='breweryLookUp'>
-                        <p> Or you can look up breweries using a zipcode. Enter a five digit zipcode below and click the button to get started.</p>
-                        <form>
-                            <input type='number' id='userEnteredZipcode' placeholder='Zipcode' /><br/>                            
-                        </form>
-                        <button type='button' className='findBreweryByInputButton' onClick={this.handleBreweryFindByUserInput.bind(this)}>Find Breweries By Zip</button>
-                    </div>
+export default function BreweryFinder(props){
+    return(
+        <div>
+            <div className='lookupContainer'>
+                <div className='autoBreweryFinder'>
+                    <p id='autoBreweryFinderText'>QUICK! Lets find a brewery close to you right...</p>
+                    <button className='quickFindButton' type='button' onClick={props.handleBreweryQuickFind}> NOW!</button>
+                </div>
+                <div className='breweryLookUp'>
+                    <p> Or you can look up breweries using a zipcode. Enter a five digit zipcode below and click the button to get started.</p>
+                    <form>
+                        <input type='number' id='userEnteredZipcode' placeholder='Zipcode' /><br/>                            
+                    </form>
+                    <button type='button' className='findBreweryByInputButton' onClick={props.handleBreweryFindByUserInput}>Find Breweries By Zip</button>
                 </div>
             </div>
-        );
-    };
+        </div>
+    );
 };
-export default BreweryFinder;
